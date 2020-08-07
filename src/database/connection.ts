3f032fce16912528c1e84e7ec4e50650cd3b5eba
Eduardo Silva <eduardo.silva@fireworkweb.com>
2020-08-07 02:09:53 -0300

    Otaku: Install Knex, Express, set blueprints + migrations, Routes

---
+++ src/database/connection.ts
@@ -0,0 +1,11 @@
+import knex from 'knex';
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'database.sqlite')
+  },
+  useNullAsDefault: true,
+});
+
+export default db;
